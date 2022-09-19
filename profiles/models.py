28189c22a01fa27803febeac96f2026e871aad66
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-09-19 23:03:04 +0000

    edit: profile view now renders form from UserProfileForm

--- profiles/models.py
+++ profiles/models.py
@@ -15,9 +15,6 @@ class UserProfile(models.Model):
     default_phone_number = models.CharField(
         max_length=20, null=True, blank=True
     )
-    default_country = CountryField(
-        blank_label='Country *', null=True, blank=True
-    )
     default_postcode = models.CharField(
         max_length=20, null=True, blank=True
     )
@@ -32,6 +29,9 @@ class UserProfile(models.Model):
     )
     default_county = models.CharField(
         max_length=80, null=True, blank=True
+    )
+    default_country = CountryField(
+        blank_label='Country', null=True, blank=True
     )
 
     def __str__(self):
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -10,9 +11,11 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(request.POST, instance=profile)
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
     }
 
     return render(request, template, context)
